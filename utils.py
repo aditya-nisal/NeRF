6804ed56c7bbd82a75163b638c042290e6ea85e8
anisal-monarch <anisal@wpi.edu>
2023-09-07 23:37:51 -0700

    updated utils.py

--- utils.py
+++ utils.py
@@ -34,3 +34,24 @@ def GetRayBundle(height, width, focal_length, T_cam2world):
     Takes: height of image, width of image, focal length of camera, camera to world transformation matrix
     Returns: Tuple containing the ray origins and directions
     '''
+
+    #COMPUTING MESHGRID OF THE PIXELS
+    w = torch.arange(width, dtype=torch.float32, device = T_cam2world.device)
+    h = torch.arange(height, dtype=torch.float32, device = T_cam2world.device)
+    w, h = torch.meshgrid(w, h)
+    ww, hh = w.transpose(-1, -2), h.transpose(-1, -2) #INTERCHANGING THE LAST AND THE SECOND TO LAST DIMENSIONS. THIS IS DONE FOR A BETTER REPRESENTATION OF A 3D POINT
+
+    #COMPUTING PIXEL DIRECTIONS. THIS IS DONE TO SET THE CENTER PIXEL TO (0, 0) AND OTHERS SYMMETRIC ESPECT TO IT 
+    pixel_directions = torch.stack(
+        [
+            (ww - width * .5) / focal_length,
+            -(hh - height * .5) / focal_length,
+            -torch.ones_like(ww),
+        ],
+        dim=-1,
+    )
+
+    #PIXEL DIRECTINS TO WORLD DIRECTIONS
+    world_directions = torch.sum(pixel_directions[..., None, :] * T_cam2world[:3, :3], dim = -1)
+    ray_origins = T_cam2world[:3, -1].expand(world_directions.shape)
+    return ray_origins, world_directions
