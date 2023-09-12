3971aee10b47e8bc4cee8d30dbf348b2b1fbcabd
anisal-monarch <anisal@wpi.edu>
2023-09-11 23:04:02 -0700

    added wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -8,3 +8,9 @@ import matplotlib.pyplot as plt
 from Utils import *
 from typing import Optional
 from Network import *
+
+
+# ALTERNATIVE IF THE DATASET IS NOT FOUND
+if not os.path.exists('tiny_nerf_data.npz'):
+    url = 'https://people.eecs.berkeley.edu/~bmild/nerf/tiny_nerf_data.npz'
+    filename = wget.download(url)
