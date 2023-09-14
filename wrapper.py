7dc64d40ee22647c30eac277791894c29ddc677a
anisal-monarch <anisal@wpi.edu>
2023-09-13 23:46:12 -0700

    updated wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -3,7 +3,7 @@
 import os
 import numpy as np
 import torch
-import wget
+import wget # pip3 install wget
 import matplotlib.pyplot as plt
 from utils import *
 from typing import Optional
@@ -29,3 +29,37 @@ def LoadData():
     focal = np.array(data["focal"])
 
     return images, poses, focal
+
+def RunIter(height, width, focal_length, tform_cam2world,
+             near_clip, far_clip, num_samples_per_ray,
+             encoding_fn, batch_fn, model):
+    
+    # COMPUTE BUNDLE RAYS THROUGH ALL THE PIXELS 
+    ray_origins, ray_directions = GetRayBundle(height, width, focal_length,
+                                                 tform_cam2world)
+
+    # GET THE SAMPLED QUERY POINTS AND DEPTH VALUES
+    query_points, depth_values = QueryPointsFromRays(ray_origins, ray_directions,
+                                                                near_clip, far_clip,
+                                                                num_samples_per_ray)
+
+    # FLATTEN 3D QUERY POINTS
+    flattened_query_points = query_points.reshape((-1, 3))
+
+    # ENCODE QUERY POINTS USING THE ENCODING FUNCTION
+    encoded_points = encoding_fn(flattened_query_points)
+
+    # SPLIT INTO MINIBATCHES AND RUN THE MODEL ON EACH MINIBATCH
+    batches = batch_fn(encoded_points, chunksize=128)
+    predictions = []
+    for batch in batches:
+        predictions.append(model(batch))
+    radiance_field_flat = torch.cat(predictions, dim=0)
+
+    # RESHAPE RADIANCE WITH AN ADDITIONAL DIMENSION OF SIZE 4 RGB AND DENSITY
+    radiance_field = torch.reshape(radiance_field_flat, list(query_points.shape[:-1]) + [4])
+
+    # SYNTHESIZE RGB IMAGE
+    rgb_predicted, _, _ = RenderVolumeDensity(radiance_field, ray_origins, depth_values)
+
+    return rgb_predicted
